9a65f62256b25977691b239acfbf8bbf192df086
Mickel99 <mickel.mounirji@gmail.com>
2024-02-12 13:15:52 +0100

    uppdatering

--- wp-content/plugins/logo-carousel-free/includes/updates/update-3.2.8.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Update version.
- *
- * @package logo-carousel-free
- */
-
-update_option( 'logo_carousel_free_version', '3.2.8' );
-update_option( 'logo_carousel_free_db_version', '3.2.8' );
